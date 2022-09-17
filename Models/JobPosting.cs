4c6b07eed61d4e370fc97c7cf8285cbf56441122
Tom Schmitz <info@tom-schmitz.de>
2022-09-17 10:14:49 +0200

    JobPosting Model wurde hinzugefügt und beschrieben

---
+++ Models/JobPosting.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Worktastic.Models
+{
+    public class JobPosting
+    {
+        public int Id { get; set; } //Primary Key
+        public string JobTitle { get; set; }
+        public string JobLocation { get; set; }
+        public string Description { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactMail { get; set; }
+        public string ContactWebsite { get; set; }
+        public byte[] CompanyImage { get; set; }
+    }
+}
