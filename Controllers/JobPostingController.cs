0db6897c48cdf5ffeb873d812c663a69d2ba5cf1
Tom Schmitz <info@tom-schmitz.de>
2022-09-18 13:37:04 +0200

    Views und Routing vorbereitung

---
+++ Controllers/JobPostingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Worktastic.Controllers
+{
+    public class JobPostingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateEditJobPosting(int id)
+        {
+            return View();
+        }
+    }
+}
