687b808fb3dfd1d7882f57e7889782be5033cc1c
Tom Schmitz <info@tom-schmitz.de>
2022-09-21 17:56:06 +0200

    JobPosting Modal Popup

--- Controllers/JobPostingController.cs
+++ Controllers/JobPostingController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.IO;
 using System.Linq;
 using Worktastic.Data;
