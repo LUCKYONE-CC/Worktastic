b704d12d6e568e503f497ddaa62a18b934bd62de
Tom Schmitz <info@tom-schmitz.de>
2022-09-22 20:55:21 +0200

    Erster API Endpunkt

---
+++ Controllers/ApiJobPostingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Worktastic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiJobPostingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hallo Welt!");
+        }
+    }
+}
